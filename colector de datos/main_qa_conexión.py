76a934ee81e9ac8746125b78646898d9997344f2
Luciano Zurdo <67966462+luislucianozurdogmailcom@users.noreply.github.com>
2024-08-21 21:15:03 -0300

    Delete colector de datos/main_qa_conexión.py

--- colector de datos/main_qa_conexión.py
+++
@@ -1,7 +0,0 @@
-import placa_canbus_conexion as pcc
-import utils
-
-# Se habilita el puerto para comunicación 
-utils.habilitar_puerto_linux();
-
-print(pcc.lectura_linux_qa(puerto_pc='/dev/ttyUSB0',target='3',entrada_analogica=1))
